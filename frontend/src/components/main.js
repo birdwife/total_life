fb44be0ce59514cd46a6902878c58445d96468da
birdwife <fiorantenicole@gmail.com>
2024-12-16 00:42:46 -0500

    entering appts

--- frontend/src/components/main.js
+++ frontend/src/components/main.js
@@ -1,10 +1,20 @@
 import { useEffect, useState } from "react";
 import './styles.css'
+import { M } from "vite/dist/node/types.d-aGj9QkWt";
 
 function Main() {
 
     const [data, setData] = useState([])
+    const [name, setName] = useState()
+    const [hour, setHour] = useState()
+    const [minute, setMinute] = useState()
+    const [status, setStatus] = useState()
 
+    async function insertDataFunction() {
+
+        console.log("ggg")
+
+    }
 
     async function fetchDataFunction() {
 
@@ -44,11 +54,15 @@ function Main() {
             <div className="bg-blue-200 h-[fit-content] p-12 rounded-2xl mt-12 ml-6">
                 <div className="bruno-ace-regular text-3xl p-2 mb-6">Enter an appointment</div>
                 <form>
-                    <input type="text" placeholder="Name"></input>
+                    <input type="text" placeholder="Name" className="rounded p-1" onChange={(e) => setName(e.target.value)}></input>
                     <br></br><br></br>
-                    <input type="text" placeholder="Time"></input>
+                    <input type="text" placeholder="12" size={2} className="pr-2 rounded p-1" onChange={(e) => setHour(e.target.value)}></input>
+                    <label className="px-2">:</label>
+                    <input type="text" placeholder="00" size={4} className="rounded p-1" onChange={(e) => setMinute(e.target.value)}></input>
                     <br></br><br></br>
-                    <input type="text" placeholder="Status"></input>
+                    <input type="text" placeholder="Status" className="rounded p-1" onChange={(e) => setStatus(e.target.value)}></input>
+                    <br></br><br></br>
+                    <input type="submit" value="Enter" className="flex justify-center bg-blue-400 rounded p-2 px-4 cursor-pointer hover:bg-blue-500" onClick={() => insertDataFunction()}></input>
                 </form>
             </div>
         </div>
